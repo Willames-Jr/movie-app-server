069a7713bfb03a57cb6705d4b8cea3a8f0069927
unknown <willamesj11@gmail.com>
2020-08-16 09:19:04 -0300

    add email field to user model

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
         required: true,
